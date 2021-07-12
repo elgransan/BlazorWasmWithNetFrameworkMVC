48a10ba1c14428bb7ea295572c52c27d7355bcae
Santiago Cattaneo <santiago@rd-its.com>
2021-07-12 17:48:25 -0300

    * initial test

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,7 +20,15 @@ namespace BlazorApp
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddSingleton<INavigationInterception>(new DisabledNavigationInterception());
+
             await builder.Build().RunAsync();
         }
+
+        public class DisabledNavigationInterception : INavigationInterception
+        {
+            public Task EnableNavigationInterceptionAsync() => Task.CompletedTask;
+        }
+
     }
 }
